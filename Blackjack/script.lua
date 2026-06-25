78dc7a3003766dabe8df1d8b9ab64cebf0e90bc8
003-aldienhw <aldienhaidarwahyono@gmail.com>
2026-06-25 10:51:40 +0700

    Fixing the bugs

--- Blackjack/script.lua
+++ Blackjack/script.lua
@@ -1,3 +1,4 @@
+local menu = true
 local userFirstCard = math.random(1, 11)
 local userSecondCard = math.random(1, 11)
 local userTotalCard = userFirstCard + userSecondCard
@@ -7,21 +8,24 @@ local dealerTotalCard = 0
 
 function Rules()
     if userTotalCard > 21 then
-        print("You lose. Your card: " .. userTotalCard)
+        print("You lose. Your cards exceed 21.")
+        menu = false
     elseif dealerTotalCard > 21 then
         print("You win! Dealer busts!")
+        menu = false
     elseif userTotalCard == 21 then
         print("You win! You very lucky!")
-        print("Your card: " .. userTotalCard)
+        print("Your cards: " .. userTotalCard)
+        menu = false
     elseif dealerTotalCard == 21 then
         print("You lose! Dealer got lucky!")
-        print("Your card: " .. userTotalCard)
+        print("Dealer cards: " .. dealerTotalCard)
+        print("Your cards: " .. userTotalCard)
+        menu = false
     elseif userTotalCard > 21 and (userFirstCard == 11 or userSecondCard == 11) then
         userTotalCard = userTotalCard - 10
-    else
-        print("Dealer cards: " .. dealerSecondCard)
-        print("Your current cards: " .. userTotalCard)
-        Inputs()
+    elseif dealerTotalCard > 21 and (dealerFirstCard == 11 or dealerSecondCard == 11) then
+        dealerTotalCard = dealerTotalCard - 10
     end
 end
 
@@ -32,14 +36,45 @@ function Inputs()
         userTotalCard = userTotalCard + math.random(1, 11)
         dealerSecondCard = dealerSecondCard + math.random(1, 11)
         dealerTotalCard = dealerFirstCard + dealerSecondCard
+        print("Dealer cards: " .. dealerTotalCard)
         Rules()
     elseif choice == "stay" then
-        dealerSecondCard = dealerSecondCard + math.random(1, 11)
-        dealerTotalCard = dealerFirstCard + dealerSecondCard
+        while dealerTotalCard <= 16 do
+            dealerSecondCard = dealerSecondCard + math.random(1, 11)
+            dealerTotalCard = dealerTotalCard + dealerSecondCard
+        end
+        if dealerTotalCard > 16 then
+            if dealerTotalCard == userTotalCard then
+                print("Draw!")
+                print("Your cards: " .. userTotalCard)
+                print("Dealer cards: " .. dealerTotalCard)
+                menu = false
+            elseif dealerTotalCard > userTotalCard then
+                print("You lose.")
+                print("Your cards: " .. userTotalCard)
+                print("Dealer cards: " .. dealerTotalCard)
+                menu = false
+            elseif dealerTotalCard < userTotalCard then
+                print("You win!")
+                print("Your cards: " .. userTotalCard)
+                print("Dealer cards: " .. dealerTotalCard)
+                menu = false
+            end
+        end
         Rules()
+    elseif choice == "exit" then
+        print("Thanks for playing!")
+        menu = false
+    else
+        print("Choose the right one!")
+        Inputs()
     end
 end
 
-print("Dealer card: " .. dealerFirstCard)
-print("Your current cards: " .. userTotalCard)
-Inputs()
+print("Dealer first card: " .. dealerFirstCard)
+
+while menu do
+    print("Your current cards: " .. userTotalCard)
+    Rules()
+    Inputs()
+end
